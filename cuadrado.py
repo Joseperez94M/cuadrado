68ac0599a9382e39ab44fa0efcf1cecb36f9f5a2
José Antonio Pérez Jiménez <josperjim1@alum.us.es>
2015-11-17 16:23:05 +0100

    Se ha documentado cuadrado.py
    Se ha añadido documentación a cada una una de las funciones del archivo cuadrado.py

--- cuadrado.py
+++ cuadrado.py
@@ -1,10 +1,13 @@
 class cuadrado:
 	def __init__(self, x, y):
+        """Este metodo es un constructor python"""
 		self.x = x
 		self.y = y
 
 	def area(self):
+        """Este metodo devuelve el area del cuadrado"""
 		return self.x * self.y
 
     def perimeter(self, x, y):
+        """Este metodo devuelve el perimetro del cuadrado"""
 	    return (2 * self.x) + (2 * self.y)
